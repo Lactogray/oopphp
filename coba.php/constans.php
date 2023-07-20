adc6b8b961e9f720bebe5c61e7f0bfe3e971b0bf
Lactogray <muhammadaryasetyabudi@gmail.com>
2023-07-20 15:34:15 +0800

    menambahkan file lagi

---
+++ coba.php/constans.php
@@ -0,0 +1,37 @@
+<?php
+
+
+define('NAMA', 'Muhammad Arya');
+
+echo NAMA;
+
+echo "<br>";
+
+const UMUR = 17;
+echo "<br>";
+echo UMUR;
+
+echo "<br>";
+
+
+
+
+echo __LINE__;
+
+echo "<br>";
+
+
+
+echo __FILE__;
+echo "<br>";
+
+echo __DIR__;
+echo "<br>";
+
+echo __FUNCTION__;
+echo "<br>";
+
+echo __CLASS__;
+echo "<br>";
+
+?>
